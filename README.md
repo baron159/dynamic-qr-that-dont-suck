c63c109d110d27694c97261d58633eb5dd02820a
Scott B <scott@baron.solutions>
2026-03-31 01:58:37 -0400

    added basic list comp to get the ball rolling

--- README.md
+++ README.md
@@ -3,3 +3,15 @@
 
 Doc links for later:
 `https://qr-code-styling.com`
+
+TODO
+---
+
+1) Add list comp to the QR panel to show all QRs
+2) Add Credit counter that shows used/available credits
+3) Add 'Purchase' landing page for Stripe
+4) Add endpoint that passes purchase confirmation to backend
+   - Check the purchase id
+   - Create credit under the user
+5) Create function that links the QR table to Credit table
+6) Ability to upload images to use within the QR
--- src/react-app/pages/Dashboard/QrPane.tsx
+++ src/react-app/pages/Dashboard/QrPane.tsx
@@ -1,11 +1,24 @@
-import { Container } from "@mantine/core";
+import { Container, Group, List, Text } from "@mantine/core";
 import { QrEditor } from "../../componets/qr.editor";
 
 
 export function QrPane() {
     return (
         <Container size="xl">
-            <QrEditor />
+            <Group preventGrowOverflow wrap='nowrap'>
+                <List w='30%'>
+                    <List.Item>
+                        <Text>QR 1</Text>
+                    </List.Item>
+                    <List.Item>
+                        <Text>QR 2</Text>
+                    </List.Item>
+                    <List.Item>
+                        <Text>QR 3</Text>
+                    </List.Item>
+                </List>
+                <QrEditor />
+            </Group>
         </Container>
     )
 }
